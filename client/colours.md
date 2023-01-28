24c3ab7b383c41b69cab37d99074a238de825c5a
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-01-28 19:24:38 +0200

    second commit

---
+++ client/colours.md
@@ -0,0 +1,15 @@
+Elements
+
+background - #f8f5f2
+headline - #232323
+paragraph - #222525
+button - #078080
+button-text - #232323
+
+Illustration
+
+stroke - #232323
+main - #fffffe
+highlight - #078080
+secondary - #f45d48
+tertiary - #f8f5f2
--- client/src/components/TitleBar.js
+++ client/src/components/TitleBar.js
@@ -1,7 +1,9 @@
 const TitleBar = () => {
   return (
-    <div>
-      <h2>titlebar</h2>
+    <div className="w-screen bg-blue-300">
+      <div className="text-center text-2xl text-">
+        <h1>Todo List</h1>
+      </div>
     </div>
   );
 };
