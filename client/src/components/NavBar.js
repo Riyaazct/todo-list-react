2d7ddc9a67404434e79961c6c6375be72a7c9673
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-04 22:20:24 +0200

    consfigure breakpoints for navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -11,39 +11,44 @@ const NavBar = () => {
 
   return (
     <div className="w-screen bg-blue-300 md:flex justify-around">
-      <div className="p-3">
-        <h1 className="text-2xl font-bold">My Todo App</h1>
-        <BiMenuAltRight
-          className={
-            !isActive
-              ? "absolute top-1 right-0 cursor-pointer md:hidden"
-              : "hidden"
-          }
-          onClick={handleClick}
-          size={35}
-        />
-        <MdClose
-          className={
-            isActive
-              ? "absolute top-1 right-0 cursor-pointer md:hidden"
-              : "hidden"
-          }
-          onClick={handleClick}
-          size={35}
-        />
-      </div>
+      <h1 className="p-2 text-2xl font-bold lg:mt-3 ">My Todo App</h1>
+      <BiMenuAltRight
+        className={
+          !isActive
+            ? "absolute top-1 right-0 cursor-pointer md:hidden"
+            : "hidden"
+        }
+        onClick={handleClick}
+        size={35}
+      />
+      <MdClose
+        className={
+          isActive
+            ? "absolute top-1 right-0 cursor-pointer md:hidden"
+            : "hidden"
+        }
+        onClick={handleClick}
+        size={35}
+      />
+
       <div
         className={
           isActive
             ? " text-center relative "
-            : "hidden md:flex md:p-5 md:w-auto"
+            : "hidden md:flex md:p-5 "
         }
       >
-        <li className="px-4 text-gray-900">Home</li>
-        <li className="px-4 text-gray-900">Catagories</li>
-        <li className="px-4 text-gray-900">Completed Tasks</li>
-        <li className="px-4 text-gray-900">Settings</li>
-        <li className="px-4 text-gray-900">About</li>
+        <ul className="md:flex">
+          <li className="p-1  text-gray-900 md:text-lg md:mx-5">
+            Home
+          </li>
+          <li className="p-1  text-gray-900 md:text-lg md:mx-5">
+            Catagories
+          </li>
+          <li className="p-1  text-gray-900 md:text-lg md:ml-5">
+            Completed Tasks
+          </li>
+        </ul>
       </div>
     </div>
   );
