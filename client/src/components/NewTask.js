3171b464ad9ca0daf091c0152528acbfb0e8a5ec
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-05 17:44:55 +0200

    complete new task component

--- client/src/components/NewTask.js
+++ client/src/components/NewTask.js
@@ -5,8 +5,8 @@ const NewTask = () => {
     <div className="h-screen w-auto p-4 bg-blue-500">
       <div className="w-full h-full text-center">
         <form>
-          <label className="text-lg" htmlFor="input">
-            New Task
+          <label className="text-lg font-medium" htmlFor="input">
+            Add New Task
             <input
               className="w-full h-10 rounded-md"
               type="text"
@@ -14,7 +14,9 @@ const NewTask = () => {
             />
           </label>
         </form>
-        <button className="">Add</button>
+        <button className="mt-2 w-full hover:bg-gray-800 hover:border-blue-700 hover:text-blue-700">
+          Add
+        </button>
       </div>
     </div>
   );
