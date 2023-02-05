3c840c135a37547dabcf2f9cc0f10a368550b930
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-05 15:45:20 +0200

    add input

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -2,9 +2,25 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div>
-      <label htmlFor="input"></label>
-      <input type="text" id="input" />
+    <div className="h-screen w-auto p-4 bg-blue-500">
+      <div className="w-full h-full text-center">
+        <form>
+          <label className="text-lg" htmlFor="input">
+            Enter Task
+            <input
+              className="w-full h-10 rounded-md"
+              type="text"
+              id="input"
+            />
+            <label htmlFor="category">Category </label>
+            <select name="category" id=" category">
+              <option value="" disabled selected>
+                Select category
+              </option>
+            </select>
+          </label>
+        </form>
+      </div>
     </div>
   );
 };
