39fc573b4247d655acf609389db286c9a06d2a9c
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-05 21:54:11 +0200

    update Tasks.js

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import dummyData from "../dummyData.json";
 
 const Tasks = () => {
+  const [data, setData] = useState(dummyData);
+
   return (
     <div className="w-[80%] bg-gray-200 border-gray-300 border-2 h-full m-auto rounded-xl p-4">
       <h1>Tasks</h1>
+      {/* Create cards for the tasks */}
     </div>
   );
 };
