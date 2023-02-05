f4725cf53f64f10cb15a3f93f4d9f6df0786fea4
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-05 17:30:14 +0200

    change component name, add button and a bit of styling

--- client/src/components/NewTask.js
+++ client/src/components/NewTask.js
@@ -1,28 +1,23 @@
 import React from "react";
 
-const Hero = () => {
+const NewTask = () => {
   return (
     <div className="h-screen w-auto p-4 bg-blue-500">
       <div className="w-full h-full text-center">
         <form>
           <label className="text-lg" htmlFor="input">
-            Enter Task
+            New Task
             <input
               className="w-full h-10 rounded-md"
               type="text"
               id="input"
             />
-            <label htmlFor="category">Category </label>
-            <select name="category" id=" category">
-              <option value="" disabled selected>
-                Select category
-              </option>
-            </select>
           </label>
         </form>
+        <button className="">Add</button>
       </div>
     </div>
   );
 };
 
-export default Hero;
+export default NewTask;
