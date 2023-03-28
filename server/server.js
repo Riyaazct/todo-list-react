5bba10c8884403148440f7fd62d21dbfa376837b
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-03-28 20:51:17 +0200

    add files

--- server/server.js
+++ server/server.js
@@ -2,8 +2,38 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3100;
 
+app.use(express.json());
+
+const dummyData = [
+  {
+    task: "Clean my room",
+  },
+  {
+    task: "Wash the car",
+  },
+  {
+    task: "Study some JavaScript",
+  },
+  {
+    task: "Work on my projects",
+  },
+  {
+    task: "go to gym",
+  },
+  {
+    task: "Go for a run",
+  },
+  {
+    task: "Pray",
+  },
+];
+
 app.get("/api", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/api/data", (req, res) => {
+  res.json(dummyData);
 });
 
 app.listen(port, () => {
