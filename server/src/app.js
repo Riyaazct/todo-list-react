d74f66acb6a7080e591d4edc5ef96393f795dca4
Riyaazct <riyaazct1@gmail.com>
2024-02-07 23:32:17 +0200

    update project

--- server/src/app.js
+++ server/src/app.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: isDevelopment
       ? "http://localhost:3000"
-      : "https://fullstack-todo-list-react.netlify.app",
+      : "https://todo-list-react-two-black.vercel.app",
     credentials: true,
   })
 );
