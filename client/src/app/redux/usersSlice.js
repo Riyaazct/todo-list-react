2f832e60e432c59e0b5d12c847d0103467a8dffb
Riyaazct <riyaazct1@gmail.com>
2024-02-20 16:46:00 +0200

    update userSlice

--- client/src/app/redux/usersSlice.js
+++ client/src/app/redux/usersSlice.js
@@ -5,7 +5,7 @@ import TokenService from "../services/token.service";
 const user = TokenService.getUSer();
 const initialState = {
   isLoggedIn: false,
-  userDetails: { user },
+  userDetails: user,
 };
 
 const usersSlice = createSlice({
