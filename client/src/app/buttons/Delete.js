eda9dd579996bef0e2a2bb072ef4240846f6fd94
Riyaazct <riyaazct1@gmail.com>
2024-03-03 13:48:34 +0200

    update delete task function to update to is_deleted

--- client/src/app/buttons/Delete.js
+++ client/src/app/buttons/Delete.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import api from "../services/api";
 import { FiDelete } from "react-icons/fi";
 import { fetchTasks } from "../redux/tasksSlice";
 
-const Delete = ({ data, setData, url, id, userId, getTodos }) => {
+const Delete = ({ id, userId }) => {
   const dispatch = useDispatch();
 
   const handleDelete = async () => {
     try {
-      const response = await api.delete(
-        `/tasks/delete/${id}/${userId}`
-      );
+      const response = await api.put(`/tasks/delete/${id}/${userId}`);
       dispatch(fetchTasks(userId));
 
       return response.data;
--- server/src/routes/tasks.routes.js
+++ server/src/routes/tasks.routes.js
@@ -4,7 +4,7 @@ const controller = require("../controllers/tasks.controller");
 
 router.route("/:id").get(controller.allTasks);
 router.route("/new").post(controller.addNewTask);
-router.route("/delete/:id/:user_id").delete(controller.deleteTask);
+router.route("/delete/:id/:user_id").put(controller.deleteTask);
 router.route("/update/:id/:user_id").put(controller.updateTask);
 router.route("/clear/:user_id").put(controller.clearTasks);
 
