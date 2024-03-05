6bf91a27300cf837e7045bfeaa1a69bb9fe97ee8
Riyaazct <riyaazct1@gmail.com>
2024-03-05 19:57:00 +0200

    first commit

---
+++ client/src/app/pages/CompletedTasks.js
@@ -0,0 +1,8 @@
+const CompletedTasks = () => {
+  return <div>CompletedTasks</div>;
+};
+
+export default CompletedTasks;
+
+// modify allTasks function to send either is_completed, is_archived or is_deleted tasks
+// based on data received from the client.
