7e348db2b886089854854f921b3bcb41a2a7348f
Riyaazct <riyaazct1@gmail.com>
2024-07-03 21:03:33 +0200

    update

--- server/src/app.js
+++ server/src/app.js
@@ -17,12 +17,33 @@ const userRoute = require("./routes/user.routes");
 
 app.use(express.json());
 
+// app.use(
+//   cors({
+//     origin: isDevelopment
+//       ? "http://localhost:3000"
+//       : "https://todo-list-react-two-black.vercel.app",
+//     credentials: true,
+//   })
+// );
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://todo-list-react-two-black.vercel.app",
+];
+
 app.use(
   cors({
-    origin: isDevelopment
-      ? "http://localhost:3000"
-      : "https://todo-list-react-two-black.vercel.app",
-    credentials: true,
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps, curl, etc.)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, // This is important to allow cookies to be sent
   })
 );
 
